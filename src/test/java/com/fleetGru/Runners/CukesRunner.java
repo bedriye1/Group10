066746b122c8f765291b0420def13759b92cd31b
Bedriye Şahin <hbedriyesahin@gmail.com>
2023-03-21 01:24:11 +0300

    hooks and cukesrunner file added

--- src/test/java/com/fleetGru/Runners/CukesRunner.java
+++ src/test/java/com/fleetGru/Runners/CukesRunner.java
@@ -1,5 +1,25 @@
 package com.fleetGru.Runners;
 
-public class CukesRunner {
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin =
+                {  //"pretty",
+                        "json:target/cucumber.json",
+                        "html:target/cucumber-report.html",
+                        "rerun:target/rerun.txt",
+
+                },
+
+        features ="src/test/resources/features",
+        glue     ="com/cydeo/step_definitions",
+        dryRun = false,
+        tags = ""
+)
+
+        public class CukesRunner {
 }
--- src/test/java/com/fleetGru/StepDefinitions/Hooks.java
+++ src/test/java/com/fleetGru/StepDefinitions/Hooks.java
@@ -1,4 +1,27 @@
 package com.fleetGru.StepDefinitions;
 
+import com.fleetGru.Utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 public class Hooks {
+
+        @After
+        public void teardownScenario(Scenario scenario) throws InterruptedException {
+
+            if (scenario.isFailed()){
+
+                byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenshot, "image/png", scenario.getName());
+
+            }
+
+
+            //BrowserUtils.sleep(5);
+            Thread.sleep(5000);
+            Driver.closeDriver();
+
+        }
 }
