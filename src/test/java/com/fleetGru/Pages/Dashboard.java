9a6bb6c129ebf739c63fa96f233bbd38db2216c0
Zafer Mengeloglu <fer.istatistik@gmail.com>
2023-03-21 22:28:47 +0300

    US003 Vehicle Table View
    feature, step defs created,
    pages (FleetVehicles, QuickLaunchPad, Dashboard) and cukesRunner updated by zafer

--- src/test/java/com/fleetGru/Pages/Dashboard.java
+++ src/test/java/com/fleetGru/Pages/Dashboard.java
@@ -1,6 +1,35 @@
 package com.fleetGru.Pages;
 
+import com.fleetGru.Utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Dashboard {
 
+    public Dashboard(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath ="//*[@id=\"user-menu\"]/a")
+    public WebElement profileName;
+
+    @FindBy(xpath ="//*[@id=\"user-menu\"]/ul/li[4]/a")
+    public WebElement logoutButton;
+
+    @FindBy(xpath ="//div[2]/div[1]/div/div/div[1]/div/h1")
+    public WebElement QuickLaunchpadText;
+
+    @FindBy(xpath ="//div[2]/div[1]/div/div/div[1]/div/h1")
+    public WebElement DashboardText;
+
+    @FindBy(xpath ="//*[@id=\"main-menu\"]/ul/li[2]")
+    public WebElement FleetDropDown;
+
+    @FindBy(xpath ="//*[@id=\"main-menu\"]/ul/li[2]/div/div/ul/li[3]/a/span")
+    public WebElement Vehicles;
+
+    @FindBy(xpath ="//th[.=*]")
+    public WebElement tableHeaders;
 
 }
--- src/test/java/com/fleetGru/Pages/QuickLaunchPad.java
+++ src/test/java/com/fleetGru/Pages/QuickLaunchPad.java
@@ -1,4 +1,44 @@
 package com.fleetGru.Pages;
 
+import com.fleetGru.Utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class QuickLaunchPad {
+
+    public QuickLaunchPad(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath ="//*[@id=\"user-menu\"]/a")
+    public WebElement profileName;
+
+    @FindBy(xpath ="//*[@id=\"user-menu\"]/ul/li[4]/a")
+    public WebElement logoutButton;
+
+    @FindBy(xpath ="//div[2]/div[1]/div/div/div[1]/div/h1")
+    public WebElement QuickLaunchpadText;
+
+    @FindBy(xpath ="//div[2]/div[1]/div/div/div[1]/div/h1")
+    public WebElement DashboardText;
+
+    @FindBy(xpath ="//span[@class='title title-level-1']")
+    public WebElement FleetDropDown;
+
+    @FindBy(xpath ="//div[@id='main-menu']/ul/li[1]/a[@href='#']/span[@class='title title-level-1']")
+    public WebElement FleetsQuichLaunch;
+
+    @FindBy(xpath ="//div[@id='main-menu']/ul/li[1]/div[@class='dropdown-menu-wrapper dropdown-menu-wrapper__placeholder']/div[@class='dropdown-menu-wrapper dropdown-menu-wrapper__scrollable']/ul[@class='dropdown-menu dropdown-menu-level-1 menu menu-level-1']//a[@href='entity/Extend_Entity_Carreservation']/span[@class='title title-level-2']")
+    public WebElement VehiclesDropDown;
+
+    @FindBy(xpath ="/html//div[@id='container']/div[@class='container-fluid page-title']//h1[@class='oro-subtitle']")
+    public WebElement Cars;
+
+    @FindBy(xpath ="//*[@id=\"main-menu\"]/ul/li[1]/div/div/ul/li[3]/a/span")
+    public WebElement Vehicles;
+
+    @FindBy(xpath ="//th[.=*]")
+    public WebElement tableHeaders;
+
 }
