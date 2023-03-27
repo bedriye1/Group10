07f7714c907322ce17d5948376dfcdfba2293656
ugmail <unenmail@gmail.com>
2023-03-27 21:24:25 +0300

    updated

--- src/test/java/com/fleetGru/Runners/CukesRunner.java
+++ src/test/java/com/fleetGru/Runners/CukesRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/fleetGru/StepDefinitions",
         dryRun = false,
-        tags = "@FLEET10-927"
+        tags = ""
 )
 
 public class CukesRunner {
