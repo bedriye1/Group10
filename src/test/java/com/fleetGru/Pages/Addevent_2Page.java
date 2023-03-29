c0b8a8fcc5949c56d6a2defc7dc84125d730ed3d
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2023-03-29 09:30:12 +0300

    AddEvent2stepDef updated

--- src/test/java/com/fleetGru/Pages/Addevent_2Page.java
+++ src/test/java/com/fleetGru/Pages/Addevent_2Page.java
@@ -1,14 +1,20 @@
 package com.fleetGru.Pages;
 
 import com.fleetGru.Utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class Addevent_2Page extends BasePage {
     public Addevent_2Page() {
        PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(xpath = "/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/div[3]/div[2]/div[2]/div[2]/div/table/tbody/tr[2]")
+    public WebElement anyVehicles;
+
     @FindBy(xpath = "//a[@title='Add an event to this record']")
     public WebElement addEventButton;
 
@@ -38,34 +44,24 @@ public WebElement allDayEventCheckBox;
 @FindBy(xpath = "//input[@data-name='recurrence-repeat']")
     public WebElement repeatButton;
 
-@FindBy(xpath = "//select[@id='recurrence-repeats-view163']")
-    public WebElement repeartsDropdown;
 
+
+@FindBy(xpath = "//select[@id='recurrence-repeats-view1984']")
+    public WebElement repeartsDropdown;
 
 @FindBy(xpath = "//option[.='Daily']")
     public WebElement dailyEvent;
 
-
     @FindBy(xpath = "//option[.='Weekly']")
     public WebElement weeklyEvent;
-
-
     @FindBy(xpath = "//option[.='Monthly']")
     public WebElement montlyEvent;
-
-
     @FindBy(xpath = "//option[.='Yearly']")
     public WebElement yearlyEvent;
-
-
-
-
-
     @FindBy(xpath = "//span[.='Never']")
     public WebElement neverButton;
     @FindBy(xpath = "//*[@id=\"oro_calendar_event_form-uid-64202eab5c91e\"]/fieldset/div[2]/div[2]/div[2]/div[6]/div/div[2]/div[1]/label/input")
     public WebElement neverCheckBox;
-
     @FindBy(xpath = "//span[.='After']")
     public WebElement afterButton;
     @FindBy(xpath = "//*[@id=\"oro_calendar_event_form-uid-64202eab5c91e\"]/fieldset/div[2]/div[2]/div[2]/div[6]/div/div[2]/div[2]/label/input[1]")
@@ -73,10 +69,8 @@ public WebElement allDayEventCheckBox;
 
     @FindBy(xpath = "//span[.='By']")
     public WebElement byButton;
-
     @FindBy(xpath = "//*[@id=\"oro_calendar_event_form-uid-64202eab5c91e\"]/fieldset/div[2]/div[2]/div[2]/div[6]/div/div[2]/div[3]/label/input")
     public WebElement byCheckbox;
-
     @FindBy(xpath = "//button[.='Save']")
     public WebElement saveButton;
 
--- src/test/java/com/fleetGru/StepDefinitions/AddEvent_2StepeDefinitions.java
+++ src/test/java/com/fleetGru/StepDefinitions/AddEvent_2StepeDefinitions.java
@@ -9,6 +9,7 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class AddEvent_2StepeDefinitions extends AddEventStepDefinition{
     AddEventPage addEventPage=new AddEventPage();
@@ -20,29 +21,27 @@ public class AddEvent_2StepeDefinitions extends AddEventStepDefinition{
     @When("User can go to fleet dropdown")
     public void userCanGoToFleetDropdown() {
         BrowserUtils.sleep(5);
-        dashboard.FleetDropDown.click();
+       dashboard.FleetDropDown.click();
     }
     @And("User click the Vehicles modules")
     public void userClickTheVehiclesModules() {
-        dashboard.Vehicles.click();
         BrowserUtils.sleep(5);
+        dashboard.Vehicles.click();
+
     }
     @When("User can click on any row in the table")
     public void user_can_click_on_any_row_in_the_table() {
-        BrowserUtils.sleep(5);
-        addEventPage.anyVehicles.click();
+        BrowserUtils.sleep(8);
+        addevent_2Page.anyVehicles.click();
     }
-
     @And("user can click add event button")
     public void userCanClickAddEventButton() {
         BrowserUtils.sleep(5);
         WebElement addEvent = Driver.getDriver().findElement(By.xpath("//a[@title='Add an event to this record']"));
         addEvent.click();
         BrowserUtils.sleep(5);
-        Assert.assertTrue(addEvent.isEnabled());
+
     }
-
-
     @Then("user should be able to see add event pop up")
     public void userShouldBeAbleToSeeAddEventPopUp() {
        addevent_2Page.addEventButton.click();
@@ -76,14 +75,20 @@ addevent_2Page.repeatButton.click();
     }
     @Then("user sees repeats dropdown menu")
     public void user_sees_repearts_dropdown_menu() {
+
         addevent_2Page.repeartsDropdown.click();
     }
     @And("user can repeat action by selecting Daily,Weekly,Montly and Yearly")
     public void userCanRepeatActionBySelectingDailyWeeklyMontlyAndYearly() {
-          addevent_2Page.dailyEvent.click();
-      addevent_2Page.weeklyEvent.click();
-      addevent_2Page.montlyEvent.click();
-      addevent_2Page.yearlyEvent.click();
+        Select repeatDropdown=new Select(Driver.getDriver().findElement(By.xpath("//select[@id='recurrence-repeats-view1984']")));
+        repeatDropdown.selectByVisibleText("Daily");
+          addevent_2Page.dailyEvent.isDisplayed();
+        repeatDropdown.selectByVisibleText("Weekly");
+      addevent_2Page.weeklyEvent.isDisplayed();
+        repeatDropdown.selectByVisibleText("Monthly");
+      addevent_2Page.montlyEvent.isDisplayed();
+        repeatDropdown.selectByVisibleText("Yearly");
+      addevent_2Page.yearlyEvent.isDisplayed();
     }
     @Then("user sholud see ending options as never, after,and should be clickable")
     public void user_sholud_see_ending_options_as_never_after_and_should_be_clickable() {
