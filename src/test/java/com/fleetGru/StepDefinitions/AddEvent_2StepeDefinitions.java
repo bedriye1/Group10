25fc3d67a027f95ea1c93e24972409fb942c34b6
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2023-03-29 13:29:30 +0300

    AddEvent2stepDef updated

--- src/test/java/com/fleetGru/StepDefinitions/AddEvent_2StepeDefinitions.java
+++ src/test/java/com/fleetGru/StepDefinitions/AddEvent_2StepeDefinitions.java
@@ -8,8 +8,11 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class AddEvent_2StepeDefinitions extends AddEventStepDefinition{
     AddEventPage addEventPage=new AddEventPage();
@@ -37,14 +40,16 @@ public class AddEvent_2StepeDefinitions extends AddEventStepDefinition{
     @And("user can click add event button")
     public void userCanClickAddEventButton() {
         BrowserUtils.sleep(5);
-        WebElement addEvent = Driver.getDriver().findElement(By.xpath("//a[@title='Add an event to this record']"));
-        addEvent.click();
+        addevent_2Page.addEventButton.click();
         BrowserUtils.sleep(5);
 
     }
     @Then("user should be able to see add event pop up")
     public void userShouldBeAbleToSeeAddEventPopUp() {
-       addevent_2Page.addEventButton.click();
+          String expectedTitle="Add Event";
+        String actualTitle= addevent_2Page.AddEventPopUp.getText();
+        System.out.println(actualTitle);
+        Assert.assertEquals(expectedTitle,actualTitle);
         BrowserUtils.sleep(3);
     }
     @And("User enters event title")
@@ -53,8 +58,7 @@ public class AddEvent_2StepeDefinitions extends AddEventStepDefinition{
     }
     @And("User can mark the event with any color")
     public void userCanMarkTheEventWithAnyColor() {
-        addevent_2Page.allTextcolors.click();
-        addevent_2Page.clickableColors.isEnabled();
+     addevent_2Page.colors.click();
 addevent_2Page.organizerEmail.sendKeys("mustafabaz861@gmail.com");
 addevent_2Page.organizerName.sendKeys("Mustafa BAZ");
 
@@ -66,7 +70,11 @@ addevent_2Page.organizerName.sendKeys("Mustafa BAZ");
     }
     @And("Time boxes will disappear")
     public void timeBoxesWillDisappear() {
-     addevent_2Page.timerDisappear.click();
+        BrowserUtils.sleep(2);
+        //Assert.assertFalse();//saati locate et ve is visible
+
+    // addevent_2Page.timerDisappear.isDisplayed();
+//Assert.assertEquals("display: none;",addevent_2Page.timerDisappear.getAttribute("style"));
 
     }
     @When("user clicks repeat button")
@@ -75,34 +83,34 @@ addevent_2Page.repeatButton.click();
     }
     @Then("user sees repeats dropdown menu")
     public void user_sees_repearts_dropdown_menu() {
-
-        addevent_2Page.repeartsDropdown.click();
+     addevent_2Page.repeatsDropdown.click();
     }
     @And("user can repeat action by selecting Daily,Weekly,Montly and Yearly")
-    public void userCanRepeatActionBySelectingDailyWeeklyMontlyAndYearly() {
-        Select repeatDropdown=new Select(Driver.getDriver().findElement(By.xpath("//select[@id='recurrence-repeats-view1984']")));
-        repeatDropdown.selectByVisibleText("Daily");
-          addevent_2Page.dailyEvent.isDisplayed();
-        repeatDropdown.selectByVisibleText("Weekly");
-      addevent_2Page.weeklyEvent.isDisplayed();
-        repeatDropdown.selectByVisibleText("Monthly");
-      addevent_2Page.montlyEvent.isDisplayed();
-        repeatDropdown.selectByVisibleText("Yearly");
-      addevent_2Page.yearlyEvent.isDisplayed();
-    }
-    @Then("user sholud see ending options as never, after,and should be clickable")
-    public void user_sholud_see_ending_options_as_never_after_and_should_be_clickable() {
-addevent_2Page.neverButton.isDisplayed();
-addevent_2Page.neverCheckBox.click();
-addevent_2Page .afterButton.isDisplayed();
-addevent_2Page.afterCheckBox.click();
-addevent_2Page.byButton.isDisplayed();
-addevent_2Page.byCheckbox.click();
-addevent_2Page.saveButton.click();
-//Assert.assertFalse();//saati locate et ve is visible
+    public void userCanRepeatActionBySelectingDailyWeeklyMontlyAndYearly(List<String>eventRepeats) {
+        Select select=new Select(addevent_2Page.repeatsDropdown);
+    List<WebElement>repeatDropdown=select.getOptions();
+    int i=0;
+        for (WebElement each : repeatDropdown) {
+           Assert.assertEquals(eventRepeats.get(i++),each.getText());
+                  }
 
     }
+
+    @Then("user should see ending options as never, after,and by should be clickable")
+    public void user_sholud_see_ending_options_as_never_after_and_should_be_clickable(List<String>itemslist) {
+        int i=0;
+        for (String each : itemslist) {
+           Assert.assertTrue(addevent_2Page.endsList.get(i++).getText().contains(each));
+
+        }
+        BrowserUtils.sleep(2);
+        addevent_2Page.saveButton.click();
+        }
+
     @Then("All Users can see all events on the General information page")
     public void allUsersCanSeeAllEventsOnTheGeneralInformationPage() {
+        JavascriptExecutor js=(JavascriptExecutor)Driver.getDriver();
+        js.executeScript("arguments[0].scrollIntoView(true);",addevent_2Page.activity);
+        Assert.assertTrue(addevent_2Page.eventAdded.isDisplayed());
     }
 }
