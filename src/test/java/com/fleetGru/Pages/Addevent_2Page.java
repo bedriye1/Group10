f9db4174accd054a7a978deb24bcc91e428292c9
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2023-03-29 13:29:22 +0300

    AddEvent2stepDef updated

--- src/test/java/com/fleetGru/Pages/Addevent_2Page.java
+++ src/test/java/com/fleetGru/Pages/Addevent_2Page.java
@@ -7,72 +7,57 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 public class Addevent_2Page extends BasePage {
     public Addevent_2Page() {
        PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(xpath = "/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/div[3]/div[2]/div[2]/div[2]/div/table/tbody/tr[2]")
+    @FindBy(xpath = "//td[.='Huey Harris']")
     public WebElement anyVehicles;
 
     @FindBy(xpath = "//a[@title='Add an event to this record']")
     public WebElement addEventButton;
 
-    @FindBy(xpath = "//span[@id='ui-id-2']")
+    @FindBy(xpath = "//span[@id='ui-id-2']")////span[.='Add Event']
     public WebElement AddEventPopUp;
-    @FindBy(xpath = "//input[@id='oro_calendar_event_form_title-uid-642021724c547']")
+    @FindBy(xpath ="//input[starts-with(@id,'oro_calendar_event_form_title-uid-642')] ")
     public WebElement addEventTitle;
 
-    @FindBy(xpath = "//button[@type='button'][2]")
-    public WebElement allTextcolors;
+    @FindBy(xpath = "//span[@title='#5484ED']")
+    public WebElement colors;
 
-    @FindBy(xpath = "//label[@for='oro_calendar_event_form_backgroundColor-uid-642021724c99f']")
-    public WebElement clickableColors;
-@FindBy(xpath = "//input [@type='email']")
+   @FindBy(xpath = "//input [@type='email']")
 public WebElement organizerEmail;
 
 @FindBy(xpath = "//input [@data-name='field__organizer-display-name']")
 public WebElement organizerName;
     @FindBy(xpath = "//input[@data-name='field__all-day']")
-public WebElement allDayEventCheckBox;
-
-
-
-    @FindBy (xpath = "//*[@id=\"time_selector_oro_calendar_event_form_end-uid-64202b8f36143\"]")
+    public WebElement allDayEventCheckBox;
+    @FindBy (xpath = "//input[starts-with(@id,'time_selector_oro_calendar_event_form_start-uid-642')]")
     public WebElement timerDisappear;
 
-@FindBy(xpath = "//input[@data-name='recurrence-repeat']")
+ @FindBy(xpath = "//input[@data-name='recurrence-repeat']")
     public WebElement repeatButton;
 
 
 
-@FindBy(xpath = "//select[@id='recurrence-repeats-view1984']")
-    public WebElement repeartsDropdown;
+@FindBy(xpath = "//select[starts-with(@id,'recurrence-repeats-view')]")
 
-@FindBy(xpath = "//option[.='Daily']")
-    public WebElement dailyEvent;
+    public WebElement repeatsDropdown;
 
-    @FindBy(xpath = "//option[.='Weekly']")
-    public WebElement weeklyEvent;
-    @FindBy(xpath = "//option[.='Monthly']")
-    public WebElement montlyEvent;
-    @FindBy(xpath = "//option[.='Yearly']")
-    public WebElement yearlyEvent;
-    @FindBy(xpath = "//span[.='Never']")
-    public WebElement neverButton;
-    @FindBy(xpath = "//*[@id=\"oro_calendar_event_form-uid-64202eab5c91e\"]/fieldset/div[2]/div[2]/div[2]/div[6]/div/div[2]/div[1]/label/input")
-    public WebElement neverCheckBox;
-    @FindBy(xpath = "//span[.='After']")
-    public WebElement afterButton;
-    @FindBy(xpath = "//*[@id=\"oro_calendar_event_form-uid-64202eab5c91e\"]/fieldset/div[2]/div[2]/div[2]/div[6]/div/div[2]/div[2]/label/input[1]")
-    public WebElement afterCheckBox;
+@FindBy(xpath = "//div[@class='controls recurrence-subview-control recurrence-subview-control__items']/div/label")
+public List<WebElement>endsList;
 
-    @FindBy(xpath = "//span[.='By']")
-    public WebElement byButton;
-    @FindBy(xpath = "//*[@id=\"oro_calendar_event_form-uid-64202eab5c91e\"]/fieldset/div[2]/div[2]/div[2]/div[6]/div/div[2]/div[3]/label/input")
-    public WebElement byCheckbox;
     @FindBy(xpath = "//button[.='Save']")
     public WebElement saveButton;
+
+    @FindBy(xpath = "//h4[.='Activity']")
+    public WebElement activity;
+
+    @FindBy(xpath = "//div[@class='details']")
+    public WebElement eventAdded;
 
 }
 
