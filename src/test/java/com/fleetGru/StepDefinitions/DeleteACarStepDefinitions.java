886283f173b0fa59128587aa0b5d2e7b86547790
ugmail <unenmail@gmail.com>
2023-03-31 19:07:06 +0300

    DeleteACarStepDefinitions updated

--- src/test/java/com/fleetGru/StepDefinitions/DeleteACarStepDefinitions.java
+++ src/test/java/com/fleetGru/StepDefinitions/DeleteACarStepDefinitions.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
