7bb1a92ed6c1dea68edd83a651195a1067c30321
sdaty <syoltay@gmail.com>
2023-04-01 01:02:16 +0000

    FleetVehiclesOdometer.java is edited

--- src/test/java/com/fleetGru/Pages/FleetVehiclesOdometer.java
+++ src/test/java/com/fleetGru/Pages/FleetVehiclesOdometer.java
@@ -1,4 +1,24 @@
 package com.fleetGru.Pages;
 
+import java.lang.invoke.SwitchPoint;
+
 public class FleetVehiclesOdometer {
+
+	public int stringToInt (String text){
+		int x = 0;
+		switch (text){
+			case "Between":
+				x = 0;
+				break;
+			case "Not Between":
+				x = 1;
+				break;
+			case "Less Than":
+				x = 5;
+				break;
+		}
+
+		return x;
+	}
+
 }
