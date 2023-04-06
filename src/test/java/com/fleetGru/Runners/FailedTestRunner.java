3a5d8561e5016df10f5c0b1d48f0588c749ffbc7
Sedat Y <syoltay@gmail.com>
2023-04-06 16:20:46 +0300

    adding some locators and plugins and FailedTestRunner.java

---
+++ src/test/java/com/fleetGru/Runners/FailedTestRunner.java
@@ -0,0 +1,17 @@
+package com.fleetGru.Runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+
+		features ="@target/rerun.txt",
+		glue     ="com/fleetGru/StepDefinitions"
+
+)
+
+public class FailedTestRunner {
+}
